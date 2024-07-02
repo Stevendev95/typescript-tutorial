f44e77762f98139baa748be42e5167f1b188b8de
Stevendev95 <steven.zuluaga@riwi.io>
2024-07-02 09:32:07 -0500

    explicit types

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,12 @@
 "use strict";
-// Arrays
-let names = ["steven", "pedro", "jairo"];
-names.push("Ana");
-// names.push(2);
-// names.push(false);
-names.forEach((name) => {
-    console.log(name.toUpperCase().length);
-});
-let numbers = [1, 4, 6];
-numbers.push(0);
-numbers.push(2);
-let mixed = ["Steven", 23];
-//objets
-let user = {
-    firstName: "Juan",
-    lastName: "Perez",
-    age: 25,
-};
-console.log(user);
+// explict types
+let age = 20;
+let user;
 user = {
     firstName: "Steven",
-    lastName: "Zuluaga",
-    age: 90,
+    lastName: "Zuluaga Cortes",
+    age: 28,
+    isStudent: true,
+    skills: ["React JS", "Next JS"],
 };
+console.log(user);
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,21 @@
-// Arrays
-let names: string[] = ["steven", "pedro", "jairo"];
+// explict types
 
-names.push("Ana");
-// names.push(2);
-// names.push(false);
+let age: number = 20;
 
-names.forEach((name) => {
-  console.log(name.toUpperCase().length);
-});
-
-let numbers: number[] = [1, 4, 6];
-
-numbers.push(0);
-numbers.push(2);
-
-let mixed: (string | number)[] = ["Steven", 23];
-
-//objets
-
-let user = {
-  firstName: "Juan",
-  lastName: "Perez",
-  age: 25,
+let user: {
+  firstName: string;
+  lastName: string;
+  age: number;
+  isStudent: boolean;
+  skills: string[];
 };
-
-console.log(user);
 
 user = {
   firstName: "Steven",
-  lastName: "Zuluaga",
-  age: 90,
+  lastName: "Zuluaga Cortes",
+  age: 28,
+  isStudent: true,
+  skills: ["React JS", "Next JS"],
 };
+
+console.log(user);
